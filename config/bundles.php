e51d9d072cc9b97f065bc37326c35fa7d152a79a
Jnah Redouane <macbookpro@MacBook-Pro-de-MacBook.local>
2023-05-19 12:47:56 +0100

    working on user

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
