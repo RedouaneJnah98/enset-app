8d310d8fb1a64e4a9bb67283d8ca0ea8acf106a6
Jnah Redouane <macbookpro@MacBook-Pro-de-MacBook.local>
2023-05-29 11:03:00 +0100

    add security bundle

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,15 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class SecurityController extends AbstractController
 {
+//    private $verifyEmailHelper;
+//    private $mailer;
+//    private Security $security;
+//
+//    public function __construct(VerifyEmailHelperInterface $verifyEmailHelper, MailerInterface $mailer, Security $security)
+//    {
+//        $this->verifyEmailHelper = $verifyEmailHelper;
+//        $this->mailer = $mailer;
+//        $this->security = $security;
+//    }
+
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
         return $this->render('security/login.html.twig');
+    }
+
+    #[Route('/email_confirmation', name: 'app_email_confirmation')]
+    public function confirmationEmail(): Response
+    {
+        return $this->render('security/confirmation_email.html.twig');
     }
 
     #[Route('/logout', name: 'app_logout')]
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,9 +3,12 @@
 namespace App\Security;
 
 use App\Repository\UserRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -15,30 +18,31 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     private RouterInterface $router;
     private UserRepository $userRepository;
+    private VerifyEmailHelperInterface $verifyEmailHelper;
+    private MailerInterface $mailer;
+    private Security $security;
 
-    public function __construct(RouterInterface $router, UserRepository $userRepository)
+    public function __construct(RouterInterface $router, UserRepository $userRepository, VerifyEmailHelperInterface $verifyEmailHelper, MailerInterface $mailer, Security $security)
     {
         $this->router = $router;
         $this->userRepository = $userRepository;
+        $this->verifyEmailHelper = $verifyEmailHelper;
+        $this->mailer = $mailer;
+        $this->security = $security;
     }
 
     public function authenticate(Request $request): Passport
     {
         $username = $request->get('username');
         $password = $request->get('password');
-        // $user = $this->userRepository->find(2);
 
-        // dd($user->getFirstName());
 
-        // dd(new PasswordCredentials($password));
-
-//        dd($password);
-//        dd($username);
         return new Passport(
             new UserBadge($username),
             new PasswordCredentials($password),
@@ -53,8 +57,22 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
-//        dd('yess');
+        $user = $this->security->getUser();
+
+        $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            'app_email_confirmation',
+            $user->getId(),
+            $user->getEmail()
+        );
+
+        $email = new TemplatedEmail();
+        $email->from('send@example.com');
+        $email->to($user->getEmail());
+        $email->htmlTemplate('security/confirmation_email.html.twig');
+        $email->context(['signedUrl' => $signatureComponents->getSignedUrl()]);
+
+        $this->mailer->send($email);
+
         return new RedirectResponse(
             $this->router->generate('app_dashboard')
         );
